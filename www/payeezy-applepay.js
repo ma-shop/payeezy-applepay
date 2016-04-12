fd4489ab1f42152f87974ffac16f4e087d735def
Trevor Brindle <trevorb@marketamerica.com>
2016-04-12 11:46:21 -0400

    update JS cordova stubs

--- www/payeezy-applepay.js
+++ www/payeezy-applepay.js
@@ -2,14 +2,26 @@ var exec = require('cordova/exec');
 
 var ApplePay = {
 
-    setMerchantId: function(successCallback, errorCallback, merchantId) {
-        exec(successCallback, errorCallback, "ApplePay", "setMerchantId", [merchantId]);
+    /**
+     * @method canMakePayments
+     * @description - Detect if iPhone supports Apple Pay and if there is a card available.
+     * @param successCallback - Function  
+     * @param errorCallback - Function  
+     **/
+    canMakePayments: function(successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'ApplePay', 'canMakePayments');
     },
 
-    makePaymentRequest: function(successCallback, errorCallback, order) {
-        exec(successCallback, errorCallback, "ApplePay", "makePaymentRequest", [order]);
+    /**
+     * @method makePaymentRequest
+     * @description - Create payment view controller with order details and given API info
+     * @param successCallback - Function  
+     * @param errorCallback - Function  
+     * @param details - Object 
+     **/
+    makePaymentRequest: function(successCallback, errorCallback, details) {
+        exec(successCallback, errorCallback, 'ApplePay', 'makePaymentRequest', [details]);
     }
-
 };
 
 module.exports = ApplePay;
